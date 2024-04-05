c99b4f2b15505f14174d49513e2db47f4ce959a0
Kalaivathanan1997 <kalaivathanan4@gmail.com>
2024-04-05 22:33:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+// import TicTacToe from './components/TicTacToe/TicTacToe'
+const App = () => {
+  return (
+    <div>
+      {/* <TicTacToe /> */}
+    </div>
+  )
+}
+
+export default App
